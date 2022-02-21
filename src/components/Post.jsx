94786dfb3c1c9549c6a64d50093f83d93d8fc3ca
Eri Kim <erikim0413@gmail.com>
2022-02-21 10:16:10 -1000

    exhibit page updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import "./Post.css";
-const Post = ({ post: { title, body,
-imgUrl, author }, index }) => {
-  return (  
-    <div className="post-container">
-      <h1 className="heading">{title}</h1>
-      <img className="image" src={imgUrl} alt="post" />
-      <p>{body}</p>
-      <div className="info">      
-        <h4>Written by: {author}</h4>
+const Post = ({ post: { title, body, imgUrl, author }, index }) => {
+  // return (
+  //   <div className="post-container">
+  //     <img className="image" src={imgUrl} alt="post" />
+  //     <h1 className="heading">{title}</h1>
+  //     {body}
+  //     <div className="info">
+  //       <h4>Written by: {author}</h4>
+  //     </div>
+  //   </div>
+  // );
+  if (index % 2 === 1) {
+    return (
+      <div className="row">
+        <div className="col">
+          <img className="image" src={imgUrl} alt="post" />
+        </div>
+        <div className="col">
+          <div className="text">
+            <h1 className="heading">{title}</h1>
+            {body}
+            <div className="info">
+              <h4>Written by: {author}</h4>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="row">
+        <div className="col">
+          <div className="text">
+            <h1 className="heading">{title}</h1>
+            {body}
+            <div className="info">
+              <h4>Written by: {author}</h4>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <img className="image" src={imgUrl} alt="post" />
+        </div>
+      </div>
+    );
+  }
 };
-  
+
 export default Post;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,85 +5,224 @@ import Post from "./Post";
 const Posts = () => {
   const blogPosts = [
     {
+      name:"pollination",
       title: "Pollination",
-      body: `Why is pollination important 
-      Its helps maintain the ecosystem
-      One in three bites of food eaten depends on pollinators 
-      What is pollination 
-      The essential part of plant reproduction
-        Pollen from the male part of the plant rubs or drops onto a pollinator, the polimator then takes it to another flower where the pollen sticks to the female part of the plant. Fertilizes the pant 
-      When does pollination happen 
-      Happens year round 
-      spring : pollinators need early blooming plants 
-      Summer: peak blooming when pollinators reach peak populations. 
-      Fall: late blooming plants for pollinators before pollinators 
-      Winter: leave decaying plants alone
-      Where do pollinators live
-      Bees can use mud, sand, plant resins and abandoned snail shells as nests.
-      Pollinators' habitats need to be within easy range for food and clean shallow water.
-      Who pollinates
-      Plants and pollinators evolved side by side for millions of years
-      Plants have developed many complex ways of attracting pollinators
-      Similarity pollinators evolved to specialized physical traits and behaviors that enhanced theri pollination efforts 
-      Bees: Flower nectar provides fuel. Pollen provides protein and amino acids. 
-      Beetles: They are “mess and soil” pollinators. They stumble though blossoms for food. Most frequently look for magnolias and flowers close to the ground 
-      Butterflies:  visit flowers with flared petals that lead to narrow throats that conceal nectar. They use their long proboscis to reach the nectar. 
-      Flies: act opposite to bees who visit sweet-smelling flowers. They are attracted to flowers with rotting odors, meat-like color, or fur-like texture. This is why they are attracted to dead animals and poop.
-      Hummingbirds: uses its long thin bill and tongue to reach the nectar hidden deep in the tubular flowers. 
-      Moths: outnumber butterflies 10 to 1. Most active at night. Night blooming flowers have a sweet scent and white or cream color blossom. 
-      Wind: pollinate grains mostly nuts and trees that blows the pollen plant to plant 
-      `,
+      body: (
+        <>
+           <h2>What is pollination?</h2>
+          <ul>
+            <li>The essential part of plant reproduction</li>
+            <li>
+              Pollen from the male part of the plant rubs or drops onto a
+              pollinator, the pollinator then takes it to another flower where
+              the pollen sticks to the female part of the plant. Fertilizes the
+              plant.
+            </li>
+          </ul>
+          <h2>Why is pollination important?</h2>
+          <ul>
+            <li>It helps maintain the ecosystem.</li>
+            <li>One in three bites of food eaten depends on pollinators.</li>
+          </ul>
+         
+          <h2>When does pollination happen?</h2>
+          <ul>
+            <li>Happens year round </li>
+            <ul>
+              <li>
+                <strong>Spring:</strong> pollinators need early blooming plants
+              </li>
+              <li>
+                <strong>Summer:</strong> peak blooming when pollinators reach
+                peak populations.
+              </li>
+              <li>
+                <strong>Fall:</strong> late blooming plants for pollinators
+                before pollinators
+              </li>
+              <li>
+                <strong>Winter:</strong> leave decaying plants alone
+              </li>
+            </ul>
+          </ul>
+          <h2>Where do pollinators live?</h2>
+          <ul>
+            <li>
+              Bees can use mud, sand, plant resins and abandoned snail shells as
+              nests.
+            </li>
+            <li>
+              Pollinators' habitats need to be within easy range for food and
+              clean shallow water.
+            </li>
+          </ul>
+          <h2>Who pollinates?</h2>
+          <ul>
+            <li>
+              Plants and pollinators evolved side by side for millions of years.
+            </li>
+            <li>
+              Plants have developed many complex ways of attracting pollinators.
+            </li>
+            <ul>
+              <li>
+                Similarity pollinators evolved to specialized physical traits
+                and behaviors that enhanced their pollination efforts.
+              </li>
+              <ul>
+                <li>
+                  <strong>Bees:</strong> Flower nectar provides fuel. Pollen provides protein and
+                  amino acids.
+                </li>
+                <li>
+                  <strong>Beetles:</strong> They are “mess and soil” pollinators. They stumble
+                  though blossoms for food. Most frequently look for magnolias
+                  and flowers close to the ground
+                </li>
+                <li>
+                  <strong>Butterflies:</strong> visit flowers with flared petals that lead to
+                  narrow throats that conceal nectar. They use their long
+                  proboscis to reach the nectar.
+                </li>
+                <li>
+                  <strong>Flies:</strong> act opposite to bees who visit sweet-smelling flowers.
+                  They are attracted to flowers with rotting odors, meat-like
+                  color, or fur-like texture. This is why they are attracted to
+                  dead animals and poop.
+                </li>
+                <li>
+                  <strong>Hummingbirds:</strong> uses its long thin bill and tongue to reach the
+                  nectar hidden deep in the tubular flowers.
+                </li>
+                <li>
+                  <strong>Moths:</strong> outnumber butterflies 10 to 1. Most active at night.
+                  Night blooming flowers have a sweet scent and white or cream
+                  color blossom.
+                </li>
+                <li>
+                  <strong>Wind:</strong> pollinate grains mostly nuts and trees that blows the
+                  pollen plant to plant
+                </li>
+              </ul>
+            </ul>
+          </ul>
+        </>
+      ),
       author: "WPI Bee Team",
-      imgUrl:
-        "image/pollination1.png",
+      imgUrl: "image/pollination1.png",
     },
     {
-      title: "Life cycle of bees ",
-      body: `There are many real-life examples of 
-      a stack. Consider an example of plates stacked 
-      over one another in the canteen. The plate 
-      which is at the top is the first one to be 
-      removed, i.e. the plate which has been placed 
-      at the bottommost position remains in the 
-      stack for the longest period of time. So, it 
-      can be simply seen to follow LIFO(Last In 
-      First Out)/FILO(First In Last Out) order.`,
-      author: "Suresh Kr",
-      imgUrl:
-        "image/lifecycle1.png",
+      title: "Life cycle of bees",
+      body: (
+        <>
+        <h2>Life Cycle of a Honey Bee (Four Stages)</h2>
+        <h3>Egg</h3>
+        <ul>
+            <li>The eggs are about half the size of a grain of rice.</li>
+            <li>
+            The queen lays the eggs in their own hexagonal cells in the brood section of the comb, attached to the cap at the end.</li>
+            <li>Eggs take about 3 days to hatch into larvae.</li>
+          </ul>
+
+          <h3>Larvae</h3>
+        <ul>
+            <li>Larvae grow in uncapped cells, which are referred to as “open brood”.</li>
+            <li>
+            The larvae are white and lay in a crescent in the cell.</li>
+            <li>When larvae are ready to molt into pupae, they stand upright in the cell and workers cap the cells with wax.</li>
+            <li>The larval stage lasts 5.5-6.5 days depending on whether they are workers, drones, or queens.</li>
+          </ul>
+
+          <h3>Pupae</h3>
+        <ul>
+            <li>Larvae molt into pupae inside the capped cells. These are referred to as “capped brood”.</li>
+            <li>
+            When the pupae molt into adult bees, they chew their way out of the capped cells.</li>
+            <li>Time in this stage varies between 8 and 14.5 days depending on whether they are workers, drones, or queens.</li>
+          </ul>
+
+          <h3>Adult Bee</h3>
+        <ul>
+            <li>Adults have a head, thorax, and abdomen, and they are covered in hairs.</li>
+            <li>The head has antennae five eyes: two large compound eyes, and three tiny eyes on top of the head.</li>
+          </ul>
+
+        <h2>Types of Bees</h2>
+        <h3>Worker Bee</h3>
+        <ul>
+            <li>Worker bees in the hive are female, and non reproductive.</li>
+            <li>
+              When an egg is fertilized, it becomes a female worker bee.
+            </li>
+            <li>Workers are adapted for carrying nectar and pollen: both of their hind legs have pollen baskets.</li>
+            <li>When a worker bee stings, the stinger is torn from their abdomen.</li>
+            <li>Female larvae that are fed pollen, nectar, and brood food become workers.
+</li>
+          </ul>
+
+          <h3>Drone Bee</h3>
+          <ul>
+            <li>Drones are the only male honey bees. </li>
+            <li>
+            They are larger, and have no stinger.
+            </li>
+            <li>Drones do not collect pollen or nectar.</li>
+            <li>The only purpose of drones is to mate with the queen.</li>
+          </ul>
+
+          <h3>Queen Bees</h3>
+          <ul>
+            <li>Female larvae that are fed royal jelly, pollen, and nectar become queens.</li>
+            <li>
+            The queen bee is the only reproductive female in the colony under normal circumstances.
+            </li>
+          </ul>
+
+          <p>Source: https://entnemdept.ufl.edu/creatures/misc/BEES/euro_honey_bee.htm</p>
+
+       
+        </>
+      ),
+      author: "WPI Bee Team",
+      imgUrl: "image/lifecycle1.png",
     },
     {
       title: "Decline in pollinator diversity and its impact",
-      body: `The word Algorithm means “a process 
-      or set of rules to be followed in calculations 
-      or other problem-solving operations”. Therefore 
-      Algorithm refers to a set of rules/instructions 
-      that step-by-step define how a work is to be 
-      executed upon in order to get the expected 
-      results. `,
+      body: (
+        <>
+          <p>
+            The word Algorithm means “a process or set of rules to be followed
+            in calculations or other problem-solving operations”. Therefore
+            Algorithm refers to a set of rules/instructions that step-by-step
+            define how a work is to be executed upon in order to get the
+            expected results.
+          </p>
+        </>
+      ),
       author: "Monu Kr",
-      imgUrl:
-        "image/decline1.png",
+      imgUrl: "image/decline1.png",
     },
     {
       title: "Call to action",
-      body: `An interconnection of multiple devices, 
-      also known as hosts, that are connected using 
-      multiple paths for the purpose of sending/
-      receiving data media. Computer networks can 
-      also include multiple devices/mediums which 
-      help in the communication between two different 
-      devices; these are known as Network devices
-      and include things such as routers, switches,
-      hubs, and bridges. `,
+      body: (
+        <>
+          <p>
+            An interconnection of multiple devices, also known as hosts, that
+            are connected using multiple paths for the purpose of sending/
+            receiving data media. Computer networks can also include multiple
+            devices/mediums which help in the communication between two
+            different devices; these are known as Network devices and include
+            things such as routers, switches, hubs, and bridges.
+          </p>
+        </>
+      ),
       author: "Sonu Kr",
-      imgUrl:
-        "image/brochure1.png",
+      imgUrl: "image/brochure1.png",
     },
   ];
 
   return (
-    <div className="posts-container">
+    <div className="posts-container{}">
       {blogPosts.map((post, index) => (
         <Post key={index} index={index} post={post} />
       ))}
